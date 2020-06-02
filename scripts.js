8a88a0af1c6ddfdc3dc206cb26c5fdd453ef849e
Rehan Khan <rehangh8924@gmail.com>
2020-06-01 21:31:21 -0400

    implemented the feature to click and hold the mouse such that the cells are changed

--- scripts.js
+++ scripts.js
@@ -1,12 +1,30 @@
-// Feature #9: clear all cells/restore all cells to their original/initial color 
+// used to track if mouse is down
+let activeColoring = false;
 
-function clearAll(){
-    let cells = document.getElementsByTagName('td');
-    let listOfCells = [...cells];
+// sets up new cell: sets event handlers and sets class to "uncolored"
+function initializeCell(cell) {
+    // change color on click
+    cell.addEventListener("click", changeColor);
+    // give cell as class called "uncolored"
+    cell.classList.add("uncolored");
 
-    listOfCells.forEach(cell=>{
-        cell.style.backgroundColor = 'red';
-        cell.classList.add('uncolored');
-    })
+    
+/* Feature #10:
+    click and hold (mouseover) from a single cell (start) to a different cell (end) 
+    such that all affected/hovered-over cells from start to end change to the 
+    currently selected color
+*/
+
+//cells will start to change color once mousedown
+    let activeColoring = fasle;
+    cell.addEventListener("mousedown", e => {
+        coloring = true;
+    });
+
+    cell.addEventListener("mousemove", e=> {
+        if(activeColoring){
+        cell.style.backgroundColor =  currentColor;
+        cell.classList.remove("uncolored");
+        }
+    });
 }
-
